c80cfd65b5e464364eb26f5bfd12f304ee458e33
ClarkB0 <clarkb@pedarecc.sa.edu.au>
2025-09-19 00:01:51 +0000

    Added order system

--- BobaBrothers.py
+++ BobaBrothers.py
@@ -3,8 +3,8 @@
 # Cancelled, success, etc.
 
 drinks = {'Taro': 4, 'Peach': 4, 'Brown Sugar': 4, 'Coffee': 4, 'Lychee': 4, 'White Tea': 4, 'Black Tea': 4, 'Coconut': 4, 'Guava': 4, 'Almond': 4, 'Caramel': 4}
-pearls = {'Brown Sugar': 2, 'Plain': 2, 'Strawbery': 2, 'Mango': 2}
-extras = {'Grass Jelly': 2, 'Coffee Jelly': 2, 'Lychee Jelly': 2, 'Peach Jelly': 2}
+pearls = {'Brown Sugar': 2, 'Plain': 2, 'Strawbery': 2, 'Mango': 2, 'None': 0}
+extras = {'Grass Jelly': 2, 'Coffee Jelly': 2, 'Lychee Jelly': 2, 'Peach Jelly': 2, 'None': 0}
 sugar = {'1 Teaspoon': 0, '2 Teaspoons': 0, '3 Teaspoons': 0, '4 Teaspoons': 0}
 ice = {'No Ice': 0, 'Moderate Ice': 0, 'Extra Ice': 0}
 drink_parts = {'drink': list(drinks.keys()), 'pearls': list(pearls.keys()), 'extras': list(extras.keys()), 'sugar amount':list(sugar.keys()), 'ice amount': list(ice.keys())}
@@ -13,45 +13,6 @@ def display_options(options):
     for i in range(len(options)):
         print(f'{i + 1}. {options[i]}')
 
-# Add back button
-
-'''print('What would you like to do?')
-display_options(['Create new order'])
-running = True
-while running:
-    action = input()
-    print()
-    running = action != 'x'
-    making_order = True
-    if running and action == '1':
-        print('What would you like to do? ')
-        display_options(['Create new drink', 'Choose drink preset'])
-        action = input()
-        print()
-        running = action != 'x'
-        if action == '1':
-            drink_order = []
-            for i in range(len(drink_parts)):
-                if making_order:
-                    part = drink_parts[i]
-                    print(f'Select {drink_part_names[i]}')
-                    display_options(part)
-                    valid = False
-                    while not valid:
-                        try:
-                            choice = input()
-                            valid = choice == 'x'
-                            running = choice != 'x'
-                            print()
-                            print(len(part))
-                            if choice not in range(1, len(part)):
-                                print('Invalid input')
-                            else:
-                                valid = True
-                        except:
-                            print('Invalid input')'''
-
-
 def select_option(options):
     # global loop variable shenanigans later
     display_options(options)
@@ -59,16 +20,49 @@ def select_option(options):
     valid_options = [str(i) for i in range(1, len(options) + 1)]
     while not valid:
         choice = input()
-        if choice not in valid_options:
+        if choice == 'x':
+            return choice
+        elif choice not in valid_options:
             print('Invalid option')
         else:
             valid = True
             return int(choice) - 1
 
 def create_drink(parts):
+    drink = []
     for part in parts:
         print(f'Select {part}')
-        option = parts[part][select_option(parts[part])]
-        print(option)
+        option = select_option(parts[part])
+        if option == 'x':
+            return False
+        else:
+            option = parts[part][option]
+            print()
+            drink.append(option)
+    return drink
 
-create_drink(drink_parts)
+running = True
+while running:
+    print('What would you like to do?')
+    print('enter x at anytime to go back or exit')
+    action = select_option(['create new order'])
+    print()
+    if action == 'x':
+        running = False
+    else:
+        order = {'drinks': []}
+        print('Enter customer name: ')
+        order['name'] = input()
+        print()
+        add_order = True
+        while add_order:
+            print('What would you like to do?')
+            action = select_option(['create new drink', 'finish order'])
+            print()
+            if action == 0:
+                drink = create_drink(drink_parts)
+                if drink:
+                    order['drinks'].append(drink)
+            elif action == 1:
+                print(order)
+                add_order = False
